88bd9fe19baad9c93c1bee283fe6118250023109
Bradley Bartlett <bbartlett222@gmail.com>
2019-05-29 20:37:41 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -34,3 +34,19 @@ def bulk_tweet_shortener(tweets_array)
     puts word_substituter(tweet)
   end
 end
+
+def selective_tweet_shortener(tweet)
+  if tweet.chars.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).chars.length > 140
+    truncated = word_substituter(tweet).chars[0..136].push("...").join("")
+  else
+    word_substituter(tweet)
+  end
+end
